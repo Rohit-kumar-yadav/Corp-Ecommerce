11299ef1f8b186a64d7303ab12cd4d69bcf2be3b
Rohit kumar yadav <76595116+Rohit-kumar-yadav@users.noreply.github.com>
2024-07-17 21:20:29 +0000

    v 1.5

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
-import checkout from "./cart.js";
 
-// const Cart = document.querySelector(".Cart-container");
+const active = document.getElementById('active');
+const ContainerCart = document.querySelector(".Cart-container");
+const CheckoutContainer = document.querySelector(".Checkout-container");
 
 const ProductBtn = document.querySelectorAll('.p-btn');
+const checkout = document.getElementById('checkout-btn')
+const subTotal = document.getElementById('sub-total');
+
 
 ProductBtn.forEach(Add);
+
+
 
 function Add(btn) {
     btn.addEventListener('click', function () {
@@ -13,16 +19,80 @@ function Add(btn) {
     });
 }
 
+
+active.addEventListener('click', () => {
+    if (CheckoutContainer.style.display == "none" || ContainerCart.style.display == "none" || checkout.style.display=="none") {
+        CheckoutContainer.style.display = "block";
+        ContainerCart.style.display = "block"
+        checkout.style.display="block"
+    }else{
+        CheckoutContainer.style.display="none";
+    }
+})
+
+
+
+
+
+let Subtotal = 0;
+
+
 function AddToCart(btn) {
     const product = btn.closest('.product');
-    const price = product.querySelector('#price').innerText;
+    const prices = product.querySelector('#price').innerText;
+    const price = prices.substring(1, prices.length)
     const Quantity = product.querySelector('.qty').value;
-    console.log("Added to cart!");
-    console.log("Quantity: ", Quantity);
-    console.log("Price: ", price);
+    const Pname = product.querySelector('h4').innerHTML;
 
-    checkout(Quantity, price);
+
+    Subtotal += (Quantity * price);
+
+    subTotal.textContent = `Subtotal: ${Subtotal}`;
+    Added(Quantity, price, Pname);
 }
+
+
+
+function Added(Quantity, price, Pname) {
+    const cart = document.querySelector('tbody');
+    const div = document.createElement('div');
+    cart.appendChild(div)
+
+    const cartItem = document.createElement('tr');
+    cartItem.classList.add('cart-item');
+
+    cartItem.innerHTML = `
+    <td>${Pname}</td>
+    <td>${Quantity}</td>
+    <td>${price}</td>
+    <td>${Quantity * price}</td>
+    `;
+    div.appendChild(cartItem);
+
+}
+
+checkout.addEventListener('click', () => {
+    const cart = document.querySelector('tbody');
+    
+    const cartItem = document.querySelector('.cart-item');
+    if(cartItem !=null){
+        cart.innerHTML="";
+        Subtotal=0;
+        subTotal.textContent = `Subtotal: ${Subtotal}`;
+    }
+    
+
+    setTimeout(() => {
+        checkout.style.display="none"
+        CheckoutContainer.style.display = "none";
+        
+    }, 2000);
+
+    
+   
+})
+
+
 
 
 
